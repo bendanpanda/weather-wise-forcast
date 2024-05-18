c18feefecfe38f8af521efae8661f2a96d4ec372
Ivanka <162492337+bendanpanda@users.noreply.github.com>
2024-05-18 17:55:31 +0100

    Adding a live weather icon

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ function refreshWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img
+      src="${response.data.condition.icon_url}"
+      class="weather-app-icon"
+    />`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
