13fa4324bddb11707557920e4ec14a56d701290d
Ivanka <162492337+bendanpanda@users.noreply.github.com>
2024-05-25 19:00:11 +0100

    Injecting the forecast HTML from JS
    The html for the forecast is now injected using JS.
    Created a function that will manipulate the html, showing 5 days as a loop.

--- src/index.js
+++ src/index.js
@@ -56,7 +56,30 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", `Wed`, `Thu`, `Fri`, `Sat`];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+          <div class="weather-forecast">
+            <div class="forecast-day">${day}</div>
+            <div class="forecast-icon">🌤️</div>
+            <div class="forecast-temp">
+              <span class="forecast-temp-max">18°</span>
+              <span class="forecast-temp-min">12°</span>
+            </div>
+          </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+displayForecast();
